09797226c3ed48265f99e3e92b9f3e8ee4ca617c
Raphael Fettaya <raphaelfettaya@MacBook-Pro-de-mac.local>
2015-12-03 17:40:44 +0200

    Auto refresh

--- www/js/app.js
+++ www/js/app.js
@@ -269,7 +269,7 @@ app.config(function($stateProvider, $urlRouterProvider, $httpProvider, $ionicCon
 
 
   $stateProvider.state('user.foots', {
-    cache: true,
+    cache: false,
     url: '/foots',
     views: {
       'menuContent' :{
