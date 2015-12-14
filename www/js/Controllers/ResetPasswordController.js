fa8f874b3956e0978f24c6eff3c714ef3f905bfa
0ruben <ruben.sebbane@gmail.com>
2015-12-14 12:34:38 +0100

    debug

--- www/js/Controllers/ResetPasswordController.js
+++ www/js/Controllers/ResetPasswordController.js
@@ -1,9 +1,12 @@
-angular.module('resetPassword',[]).controller('ResetPasswordCtrl', function($http, $scope){
+angular.module('resetPassword',[]).controller('ResetPasswordCtrl', function($http, $scope, $validated){
 
 	$scope.sendResetMail = function(email){
 
 		$http.post(serverAddress+'/user/resetPassword',{email:email}).success(function(data){
 			$scope.erreur ="";
+			$validated.show({texte: "Nous avons envoyé des instructions à l'e-mail indiqué", icon: "ion-checkmark-round"},function(){
+				
+			});
 		}).error(function(err){
 			$scope.erreur="Nous n'avons pas trouvé de compte associé à cette adresse mail";
 		});
